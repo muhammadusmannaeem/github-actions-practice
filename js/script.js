87d6a1b5efdcf1c356fe17bb046df604e757444d
Usman-Naeem <usman.naeem2212@gmail.com>
2022-07-02 11:28:49 +0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let fulldate =  new Date();
+let year = fulldate.getFullYear();
+let month = fulldate.getMonth() + 1;
+let date = fulldate.getDate();
+
+const dayOfTheWeek = (date = new Date()) => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[date.getDay()];
+}
+
+try {
+    document.getElementById("date").innerHTML = date + " - " + month + " - " + year;
+    document.getElementById('day').innerText = dayOfTheWeek();
+} catch(err) {}
+
