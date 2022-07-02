1e1a09df716f578d7df87863920a0c137a64ea44
Usman-Naeem <usman.naeem2212@gmail.com>
2022-07-02 12:18:28 +0500

    made mistake to fail test

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let month = fulldate.getMonth() + 1;
 let date = fulldate.getDate();
 
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const days = ['Snday', 'Monday', 'Tuesday', 'Wednesday', 'Thrsday', 'Friday', 'Saturday'];
     return days[date.getDay()];
 }
 
