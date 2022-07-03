54fafa8ffd4b377408977b002e5d60109d4df432
Usman-Naeem <usman.naeem2212@gmail.com>
2022-07-03 09:35:43 +0500

    added further tests #2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 let fulldate =  new Date();
 let year = fulldate.getFullYear();
 let month = fulldate.getMonth() + 1;
-let date = fulldate.getDate();
+let day = fulldate.getDate();
 
 const dayOfTheWeek = (date = new Date()) => {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    return days[date.getDay()];
+    console.log("kk " + date);
+    if (date === parseInt(date, 10))
+        return undefined;
+    else if (date === null)
+        return undefined;
+    else if (date == null)
+        return undefined;
+    else if (date == "Invalid Date")
+        return undefined;
+    else if (date instanceof String) {
+        date = new Date(date);
+        return days[date.getDay()];
+    }
+    else if (date instanceof Date)
+        return days[date.getDay()];
+    else
+        return undefined;
 }
 
 try {
-    document.getElementById("date").innerHTML = date + " - " + month + " - " + year;
     document.getElementById('day').innerText = dayOfTheWeek();
+    document.getElementById("date").innerHTML = day + " - " + month + " - " + year;
 } catch(err) {}
 
 exports.dayOfTheWeek = dayOfTheWeek;
--- js/script.test.js
+++ js/script.test.js
@@ -1,37 +1,76 @@
 const { dayOfTheWeek } = require('./script');
 
+/*
+    Test if date is provided in proper format
+*/
 
+// For Monday
 test('getDay returns the day of the week', () => {
     const day = dayOfTheWeek( new Date('7/4/2022') );
     expect( day ).toBe('Monday');
 });
 
+// For Tuesday
 test('getDay returns the day of the week', () => {
     const day = dayOfTheWeek( new Date('7/5/2022') );
     expect( day ).toBe('Tuesday');
 });
 
+// For Wednesday
 test('getDay returns the day of the week', () => {
     const day = dayOfTheWeek( new Date('7/6/2022') );
     expect( day ).toBe('Wednesday');
 });
 
+// For Thursday
 test('getDay returns the day of the week', () => {
     const day = dayOfTheWeek( new Date('7/7/2022') );
     expect( day ).toBe('Thursday');
 });
 
+// For Friday
 test('getDay returns the day of the week', () => {
     const day = dayOfTheWeek( new Date('7/8/2022') );
     expect( day ).toBe('Friday');
 });
 
+// For Saturday
 test('getDay returns the day of the week', () => {
     const day = dayOfTheWeek( new Date('7/9/2022') );
     expect( day ).toBe('Saturday');
 });
 
+// For Sunday
 test('getDay returns the day of the week', () => {
     const day = dayOfTheWeek( new Date('7/10/2022') );
     expect( day ).toBe('Sunday');
 });
+
+/*
+    Test if date is NOT provided in proper format
+*/
+
+// For Empty date
+test('getDay returns the day of the week', () => {
+    const day = dayOfTheWeek( new Date('') );
+    expect( day ).toBe(undefined);
+});
+
+// For Null date, Returns base value:: Thu Jan 01 1970 
+test('getDay returns the day of the week', () => {
+    const day = dayOfTheWeek( new Date(null) );
+    expect( day ).toBe('Thursday');
+});
+
+// For Invalid date
+test('getDay returns the day of the week', () => {
+    const day = dayOfTheWeek( new Date(undefined) );
+    expect( day ).toBe(undefined);
+});
+
+// If number is passed as date
+test('getDay returns the day of the week', () => {
+    const day = dayOfTheWeek( 10 );
+    expect( day ).toBe(undefined);
+});
+
